fecbfc8403c119723bc6a1b9e0bbbde79929c710
faalatawi <fa.atawi@gmail.com>
2019-11-25 16:17:19 -0700

    data sets

---
+++ src/Datasets.py
@@ -0,0 +1,21 @@
+# test git
+from sklearn.datasets import fetch_20newsgroups
+import nltk
+from nltk.corpus import reuters 
+from nltk.corpus import stopwords
+import re
+
+def clean_text(text):
+    return test
+
+def get_20newsgroups():
+    pass
+
+def get_reuters():
+    pass
+
+def get_20newsgroups_vectorized():
+    pass
+
+def get_reuters_vectorized():
+    pass
